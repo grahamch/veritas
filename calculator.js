05a196e097be7184e0d5f95e83b34ef1c1ac5ec8
Graham <gcherrington@gmail.com>
2026-05-15 20:24:43 +0200

    Refactor products map to use destructuring and shorthand

--- calculator.js
+++ calculator.js
@@ -1,3 +1,4 @@
+
 function pricePerGramProtein(priceRands, totalWeightG, proteinPerServingG, servingSizeG) {
 const proteinRatio = proteinPerServingG / servingSizeG;
 const totalProteinG = totalWeightG * proteinRatio;
@@ -5,14 +6,25 @@ const costPerGram = priceRands / totalProteinG;
 return Math.round(costPerGram * 100) / 100;
 }
 
-const products = [
-    { name: "Biogen Iso Whey", cost: pricePerGramProtein(1169.99, 1500, 21, 30) },
-    { name: "NPL Platinum", cost: pricePerGramProtein(800.99, 2000, 25, 40) },
-    { name: "USN Hardcore Whey GH", cost: pricePerGramProtein(789.99, 1800, 27, 33) },
-];
+const rawData = [
+    ["Biogen Iso Whey", 1169.99, 1500, 21, 30],
+    ["NPL Platinum", 499.99, 1000, 24, 30],
+    ["USN Hardcore Whey GH", 649.99, 2000, 22, 33]
+]
 
+let products = rawData.map(([name, price, weight, protein, serving]) => ({name, cost: pricePerGramProtein(price, weight, protein, serving)
+}))
+
+for(let {name, cost} of products.filter(item => item.cost < 1)) {
+    console.log(`${name}: R${cost}`);
+}
+
+
+
+/*
 products.sort((a, b) => a.cost - b.cost); 
 
 products.forEach((p, i) => {
     console.log(`${i + 1}. ${p.name}: R${p.cost} per gram of protein`);
 });
+*/
